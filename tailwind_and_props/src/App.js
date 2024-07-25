4b2d29ac9e83c11272e191e02feb6e1326e7529d
Deepthi <deepthipurujala@gmailc.om>
2024-07-25 17:31:36 +0530

    tailwind and props

---
+++ tailwind_and_props/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+    <Card name='Deepthi' btnText='click me'/>
+    <Card name='srikar'/>
+    </>
+  );
+}
+
+export default App;
