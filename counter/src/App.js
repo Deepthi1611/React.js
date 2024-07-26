8f39c90b2216e519afcd4d0ca6c090aa997c3756
Deepthi <deepthipurujala@gmailc.om>
2024-07-26 09:59:25 +0530

    interview question on counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -14,8 +14,18 @@ function App() {
     //  To update the data in UI, we use hooks
     // UI updation is taken care by React
 
-    setCounter(counter+1) 
+    // setCounter(counter+1) 
     //  whenever counter is updated, React updates all the place wherever counter state is used
+
+    // if setCounter(counter + 1) is used successively it stills results in adding a single number to counter
+    // This is because useState uses batching and it is asynchronous in nature
+    // It batches all four statements and possibility is that it executes only single seCounter as all the statements are same
+    // useState is asynchronous, so it may not get the updated counter value while calling setCounter multiple times
+    // so, to overcome this issue, we use callback function in setCounter so that the callback function waits until the correct counter value is obtained
+    setCounter(counter => counter+1)
+    setCounter(counter => counter+1)
+    setCounter(counter => counter+1)
+    setCounter(counter => counter+1)
   }
 
   const subtractValue = () => {
